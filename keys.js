39acf958c1466c74019d613fa4b47472c43a0d5a
gurneet33 <gurneet.me@gmail.com>
2018-12-13 23:06:42 -0500

    working now

--- keys.js
+++ keys.js
@@ -1,4 +1,12 @@
-module.exports.spotify = {
-    id: '1451670fb441422db8ab4c7c839bcc8f',
-    secret: '9070d94150484d8393ea872fe240afee'
+exports.omdb = {
+    apikey: process.env.OMDB_APIKEY
+}
+
+exports.spotify = {
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+}
+
+exports.band = {
+    app_id: process.env.BAND_APP_ID
 }
--- liri.js
+++ liri.js
@@ -1,11 +1,19 @@
 var Spotify = require('node-spotify-api');
 var axios = require('axios');
+var dotenv = require('dotenv').config();
+var moment = require('moment');
+
+var fs = require('fs');
+
 var chalk = require('chalk');
 
 var log = console.log;
 
 var keys = require('./keys')
 var spotify = new Spotify(keys.spotify);
+
+var parameter = process.argv.slice(3).join(" ");
+
 
 // * `spotify-this-song`
 
@@ -17,40 +25,60 @@ log(action);
 switch (action) {
     case ("concert-this"):
         concertThis();
+        logAction();
         break;
     case ("spotify-this-song"):
         spotifyThisSong();
+        logAction();
         break;
     case ("movie-this"):
         movieThis();
+        logAction();
+        break;
+    case ("do-what-it-says"):
+        doIt();
+        logAction();
         break;
     default:
-        log("enter your choices atleast");
+        log("problem with switch1");
 }
 
 
 
-function spotifyThisSong() {
-    var songSearch = process.argv.slice(3).join(" ");
+function spotifyThisSong(song) {
+
+    if (parameter) {
+        var songSearch = parameter;
+    } else if ((!parameter) && (action === "spotify-this-song")) {
+        var songSearch = "The Sign Ace of Base";
+    } else {
+        songSearch = song;
+    }
+
+    console.log("songsearch", songSearch)
     spotify
         .search({
             type: 'track',
             query: songSearch
         })
         .then(function (response) {
-            var results = response.tracks.items[0];
-            var artist = results.artists[0].name;
-            var songName = results.name;
-            var songLink = results.external_urls.spotify;
-            var album = results.album.name;
+            // console.log(response)
+            for (var i = 0; i < 5; i++) {
+                var results = response.tracks.items[i];
+                var artist = results.artists[0].name;
+                var songName = results.name;
+                var songLink = results.external_urls.spotify;
+                var album = results.album.name;
 
-            //Need: artist(s), song's name, preview link of song, album//
-            console.log("************");
-            console.log("Artist: " + artist);
-            console.log("Song: " + songName);
-            console.log("Song Link: " + songLink);
-            console.log("Album: " + album);
-            console.log("************");
+                //Need: artist(s), song's name, preview link of song, album//
+                console.log("************");
+                console.log("Artist: " + artist);
+                console.log("Song: " + songName);
+                console.log("Song Link: " + songLink);
+                console.log("Album: " + album);
+                console.log("************");
+            }
+
 
         })
         .catch(function (err) {
@@ -58,14 +86,32 @@ function spotifyThisSong() {
         });
 }
 
-function concertThis() {
-    var band = process.argv.slice(3).join(" ");
-    axios.get("https://app.swaggerhub.com/apis/Bandsintown/PublicAPI/3.0.0" + band + "/events?app_id=5fd1b4db4b5d87de54d26c2c0a66a70c")
-        //     // var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=";
+function concertThis(band) {
+    if (parameter) {
+        var bandSearch = parameter;
+    } else {
+        bandSearch = band;
+    }
+    axios.get("https://rest.bandsintown.com/artists/" + bandSearch + `/events?app_id=${keys.band.app_id}`)
         .then(function (result) {
-            console.log(result)
-            // var results = JSON.parse(result);
-            // console.log(results);
+            for (var i = 0; i < 5; i++) {
+                // log(result.data)
+                var results = result.data[i].venue;
+                // log("results", results);
+                console.log("************");
+                console.log("Country:" + results.country +
+                    "\nCity:" + results.city +
+                    "\nVenue Location:" + results.name +
+                    "\nDate:" + moment(result.data.datetime).format("MM/DD/YYYY"))
+                console.log("************");
+            }
+
+            // result.data.map(item => {
+            //     var venue = item.venue;
+            //     // log(result.data)
+
+            // });
+
         })
         .catch(function (error) {
             console.log("where is the error", error)
@@ -73,12 +119,17 @@ function concertThis() {
 }
 
 
-function movieThis() {
-    var movie = process.argv.slice(3).join(" ");
-    axios.get("https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=3fbf992f")
-        // ("http: //www.omdbapi.com/?t=Game of Thrones&Season=1&apikey=")
-        // var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=";
+function movieThis(movie) {
+    if (parameter) {
+        var movieSearch = parameter;
+    } else if ((!parameter) && (action === "movie-this")) {
+        var movieSearch = "Mr. Nobody";
+    } else {
+        movieSearch = movie;
+    }
+    axios.get("https://www.omdbapi.com/?t=" + movieSearch + `&y=&plot=short&apikey=${keys.omdb.apikey}`)
         .then(function (result) {
+
             var data = result.data;
             log("************");
             log(chalk.red.bgWhite("Title:   "), data.Title,
@@ -89,20 +140,57 @@ function movieThis() {
                 "\nPlot:       ", data.Plot,
                 "\nActors:     ", data.Actors)
             log("************");
-            // var results = JSON.parse(result)[0];
-            // console.log(results);
+
+
         })
         .catch(function (error) {
             console.log("where is the error", error)
         })
 }
 
+function doIt() {
+    fs.readFile("random.txt", "utf8", function (error, data) {
 
-// * Title of the movie.
-//    * Year the movie came out.
-//    * IMDB Rating of the movie.
-//    * Rotten Tomatoes Rating of the movie.
-//    * Country where the movie was produced.
-//    * Language of the movie.
-//    * Plot of the movie.
-//    * Actors in the movie.
+        // If the code experiences any errors it will log the error to the console.
+        if (error) {
+            return console.log(error);
+        }
+        var randomAction = data.split(',')[0];
+        var randomSearch = data.split(',')[1];
+        console.log(randomAction);
+        console.log(randomSearch);
+
+        switch (randomAction) {
+            case ("concert-this"):
+                concertThis(randomSearch);
+                break;
+            case ("spotify-this-song"):
+                console.log("randomSearch", randomSearch)
+                spotifyThisSong(randomSearch);
+                break;
+            case ("movie-this"):
+                movieThis(randomSearch);
+                break;
+            default:
+                log("enter your choices atleast2");
+        }
+    })
+
+}
+
+function logAction() {
+    var log = action + " " + parameter + "\n ";
+    fs.appendFile("log.txt", log, function (err) {
+
+        // If an error was experienced we will log it.
+        if (err) {
+            console.log(err);
+        }
+
+        // If no error is experienced, we'll log the phrase "Content Added" to our node console.
+        else {
+            console.log("Content Added!");
+        }
+
+    });
+}
